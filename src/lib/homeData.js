074c2fbd043052e8fd70fd6c0396d7e2f21d32dd
cucereanum <cucereanum@yahoo.com>
2022-11-02 22:37:37 +0200

    Create the second section of home page

---
+++ src/lib/homeData.js
@@ -0,0 +1,27 @@
+export const secondSectionData = [
+  {
+    title: "Cursuri acreditate",
+    description:
+      "ARDOR, asociația noastră umbrelă, a dezvoltat un curs acreditat de formare profesională adresat cadrelor didactice.",
+  },
+  {
+    title: "Opțional",
+    description:
+      "Răspunde la nevoia a programei școlare de a fi reîmprospătată. Astfel, profesorii au un rol de cheie în a învăța să predea această materie.",
+  },
+  {
+    title: "Olimpiadă",
+    description:
+      "În cadrul olimpiadei naționale de dezbateri, profesorii au un rol fundamental în educarea elevilor prin calitatea de arbitru.",
+  },
+  {
+    title: "Club",
+    description:
+      "Profesorii se ocupă cu deschiderea cluburilor în școlile în care predau, cu ajutorul ARDOR Muntenia, care se ocupă de toate formele birocratice.",
+  },
+  {
+    title: "Tabăra de Leadership",
+    description:
+      "Un eveniment ARDOR, care reunește în fiecare an, în București, profesorii din întreaga țară.",
+  },
+];
